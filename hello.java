7a8fbb6a39d6a5e5d9893fd7d23d2d79e332cbdf
Adil Alahee <adilmdctg@gmail.com>
2024-09-20 11:10:19 -0700

    variables

---
+++ hello.java
@@ -0,0 +1,11 @@
+class hello
+{
+    public static void main(String a[])
+    {
+        // System.out.print("Hello World");
+        int num1=54;
+        int num2=34;
+        int result=num1+num2;
+        System.out.print(result);
+    }
+}
